c90655fd7ec92e28ebadcb6cd144885a311bdd58
Shan Yang <yangshan722@hotmail.com>
2015-09-03 16:08:32 -0700

    First commit

---
+++ blog.php
@@ -0,0 +1,28 @@
+﻿<?php
+	$section = "My Blog";
+	$extralink = "<link href=\"css/blog.css\" rel=\"stylesheet\">";
+	include("head.php");
+?>
+
+<!--
+<!DOCTYPE html>
+<html>
+<head>
+<title>Shan Yang - Days Worth Remembering</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
+</head>
+-->
+
+<p>
+4/28/2015
+作业好多。154好心塞。关键是明明很忙，却每天都想玩。到底什么情况啊。研究出来了怎么直接在当前页面切换section，说是直接用php的include就好了。昨天弄了很久网页，今天要心塞写作业啦！
+</p>
+<p>
+4/26/2015
+昨天申请了域名，今天整了一下午这个网页的设计。似乎想要的设计还是需要javascript才能实现。明后天去问问助教有没有简单的方法。
+</p>
+
+
+<?php
+	include("tail.html");
+?>
---
+++ head.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Shan Yang - <?= $section ?></title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
+<link href="css/customer-comp.css" rel="stylesheet">
+<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Oleo+Script" rel="stylesheet" type="text/css">
+<?= $extralink ?>
+
+</head>
+
+<body>
+	<h1>Shan Yang</h1>
+	<hr />
+	<div id="navcontainer">
+		<ul>
+			<li id="front"><a href="index.html"><img src="images/icon/1.png" alt="front" /> Front Page </a></li>
+			<li id="resume"><a href="resume.php"><img src="images/icon/2.png" alt="resume" /> Resume </a></li>
+			<li id="project"><a href="#"><img src="images/icon/3.png" alt="project" /> Projects </a></li>
+			<li id="blog"><a href="blog.php"><img src="images/icon/4.png" alt="blog" /> Blog </a></li>
+			<li id="contact"><a href="#"><img src="images/icon/5.png" alt="contact" /> Contact Me </a></li>
+		</ul>
+	</div>
+	<hr />
+	<div class="white" id="para">
